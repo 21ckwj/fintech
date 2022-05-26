9d4e4d80c4c9aaff533cf0af86f8009c8e5ecfe9
21ckwj <21ckwj@naver.com>
2022-05-26 20:50:10 +0900

    pj6_0526

---
+++ Flask&PyQt/pyqt_1.py
@@ -0,0 +1,21 @@
+from email.charset import QP
+import sys
+from prompt_toolkit import Application
+import PyQt5
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class MainDialog(QDialog):
+    def __init__(self):
+        QDialog.__init__(self, None)
+        self.lineEdit = QLineEdit(self)
+        self.pushButton = QPushButton(self)
+        self.pushButton.move(0,100)
+
+app = QApplication(sys.argv)  # 여기서 부터 app.exec_() 사이에 실제 실행(이벤트루프)될 코드가 나온다
+main_dialog = MainDialog()
+main_dialog.show()
+
+app.exec_() # 프로그램을 이벤트 루프로 진입시키는 메소드 - window 프로그램 특징
+            # 이벤트 루프: 프로그램이 종료되지 않고 다음 명령(이벤트)를 대기하는 상태
---
+++ Flask&PyQt/st_chat_연습.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from streamlit_chat import message
+
+message("Hi 고슴도치 봇입니다") 
+message("Hello 고슴도치!", is_user=True) 
+message("이지혜 누나 담배그만펴야 하겠지?") 
+message("당연하지!", is_user=True) 
